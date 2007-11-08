c851c45a71a03bd5adc1984b5603bef937dfb05b
KOSEKI Kengo <koseki@gmail.com>
2007-11-08 09:16:45 +0000

    全体を書き直しました。個々のノードをハッシュで扱うデータ構造に変えました。

--- public/static/js/tree/mktreebyarray2.js
+++ public/static/js/tree/mktreebyarray2.js
@@ -1,94 +1,92 @@
-// based on http://allabout.co.jp/internet/javascript/closeup/CU20060530A/index.htm
+// http://developer.yahoo.com/yui/examples/treeview/dynamic_tree.html
+// http://allabout.co.jp/internet/javascript/closeup/CU20060530A/index.htm
 
-YAHOO.namespace('smc');
-YAHOO.smc.tree = function(id) {
-
+YAHOO.namespace('jsontree');
+YAHOO.jsontree.JsonTree = function(id) {
+  this.id = id;
   this.tree = new YAHOO.widget.TreeView(id);
+  this.node_count = 0;
+};
   
-  YAHOO.smc.tree.prototype.mkTreeByArray = function (treeData,treeNode){
-    if (!treeNode) {
-      treeNode = this.tree.getRoot(); 
-    }
+YAHOO.jsontree.JsonTree.prototype.build = function(data, parentNode) {
+  if (parentNode === undefined) {
+    parentNode = this.tree.getRoot();
+  }
+  for (var i = 0; i < data.length; i++) {
+    this.buildNode(data[i], parentNode);
+  }
+  this.tree.draw();
+};
 
-    for (var i in treeData) {
-      var tmpData = treeData[i];
-      var key = tmpData[0];
-      if (! key || key == "_open" || key == "_close" || key == "_load") {
-      	continue;
-      }
-
-      var tmpNode = new YAHOO.widget.TextNode("" + key, treeNode, false);
-      var nodeData = tmpData[1];
-      if(typeof nodeData == "string") { 
-        tmpNode.href= nodeData;
-        tmpNode.target= "sample"; 
-      } else if (typeof nodeData == "object") {
-      	var nextTreeData = nodeData;
-      	
-        this.mkTreeByArray(nextTreeData, tmpNode); 
-        switch (nextTreeData[0][0]) {
-          case "_open": 
-          	tmpNode.expand();
-          	break;
-          case "_close":
-            tmpNode.collapse();
-            break;
-          case "_load": 
-            YAHOO.smc.loadTreeData(this, tmpNode, tmpData);
-            break;
-          case "_exec":
-          dafault:
-            tmpNode.expand();
-            break;
-        }
-      }
-    }
-    this.tree.draw();
-  };
-}
-
-YAHOO.smc.loadTreeData = function(oj, tmpNode, treeDataFrg) {
-  if (! YAHOO.util.Connect) {
+YAHOO.jsontree.JsonTree.prototype.buildNode = function(data, parentNode) {
+  if (data.label === undefined) {
     return;
   }
+
+  var param = {
+    "id": "node" + this.id + "_" + this.node_count,
+    "label": data.label
+  };
+  this.node_count++;
   
-  var d = treeDataFrg[1][0][1];
-  if (d) {
-    tmpNode.method = d.method ? d.method : "GET";
-    tmpNode.url = d.url ? d.url : "";
+  var node = new YAHOO.widget.TextNode(param, parentNode, data.open);
+  node.jsontree = data;
+  node.jsontree.linked = false;
+
+  if (data.command == "load") {
+    this.initDynamicNode(data, node);
+  } else if (data.command) {
+    // TODO
+  }
+
+  if (data.href) {
+    node.href = data.href;
+    node.jsontree.linked = true;
+  }
+  if (data.target) {
+    node.target = data.target;
+  }
+
+  if (data["."] !== undefined) {
+    this.build(data["."], node);
   }
   
-  var f = function (node, onCompleteCallback) {
-    tmpNode = new YAHOO.widget.Node("", tmpNode.pearent, false);
-    var delay = YAHOO.smc.loadTreeData.delay;
-    if (0 < delay) {
-      setTimeout(onCompleteCallback, delay);
-    } else {
-      onCompleteCallback();
-    }
-  };
-  tmpNode.setDynamicLoad(f);
-      
-  oj.tree.onExpand = function(node) {
-	/*
-    if (tmpNode.label != node.label) {
-      return;
-    }
-    */
-    if (node.children.length <= 0) {
-      var opts = { 
-        argument: {'node': node},
-        scope: oj, 
-        success: getResponse 
-      };
-      YAHOO.util.Connect.asyncRequest(node.method, node.url, opts, null);
-    }
-  };
+};
 
-  getResponse = function(oj) {
-    data = eval(oj.responseText);
-    this.mkTreeByArray(data, oj.argument.node);
-  };
-}
+YAHOO.jsontree.JsonTree.prototype.initDynamicNode = function(data, node) {
+  if (! YAHOO.util.Connect) {
+  	alert("YAHOO.util.Connect required!");
+    return;
+  }
 
-YAHOO.smc.loadTreeData.delay = 0;
+  var config = {
+  	"url": data.src,
+  	"method": data.method ? data.method : "GET"
+  }
+  node.jsontree.xhr_config = config;
+
+  var jsontree = this;
+  var loadNodeData = function(node, fnLoadComplete)  {
+    var opts = { 
+      success: function(oResponse) {
+	    var data = eval("(" + oResponse.responseText + ")");
+	    var arg = oResponse.argument;
+		jsontree.build(data, arg.node);
+		arg.fnLoadComplete();
+      },
+      failure: function(oResponse) { 
+        // YAHOO.log("Failed to process XHR transaction.", "info", "example"); 
+        oResponse.argument.fnLoadComplete(); 
+      },
+      argument: {
+      	"node": node,
+      	"fnLoadComplete": fnLoadComplete
+      },
+      timeout: 7000
+    };
+
+    var conf = node.jsontree.xhr_config;
+    YAHOO.util.Connect.asyncRequest(conf.method, conf.url, opts, null);
+  }
+  node.setDynamicLoad(loadNodeData);
+};
