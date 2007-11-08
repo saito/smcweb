10e1c09a5bf88fff53409d9950ddefd8a28d3c6e
KOSEKI Kengo <koseki@gmail.com>
2007-11-08 09:19:10 +0000

    ツリーを表現するJSONのデータ構造を変えました。メインのフレームにコンテンツを表示しました。

--- app/controllers/browser_controller.rb
+++ app/controllers/browser_controller.rb
@@ -2,31 +2,53 @@ class BrowserController < ApplicationController
 
   before_filter :read_site_config_filter
   attr_reader :config
-
+  
   require 'pp'
-
-private
+  require 'pathname'
 
   def index
   end
   
   def tree
-    @root = config["root"]
-    @path = ""
+    @root = root
+    @path = path
   end
   
   def tree_json
-    @root = config["root"]
-    if params[:path].is_a? Array
-      @path = params[:path].join("/")
-    else
-      @path = params[:path].to_s
-    end
+    @headers["content-type"] = "text/javascript+json; charset=utf-8"
+    # @headers["content-type"] = "text/plain; charset=utf-8"
+    @root = root
+    @path = path
   end
 
 private
 
+  def root
+    return realpath(config["root"], Pathname.new("/"))  
+  end
+
+  def path
+    if params[:path].is_a? Array
+      path = params[:path].join("/")
+    else
+      path = params[:path].to_s
+    end
+    path = realpath(@root + path, @root)
+    return path
+  end
+
+  def realpath(path, parent)
+    path = Pathname.new(path)
+    raise "path not found: #{path}" unless path.exist?
+    path = path.realpath
+    
+    parent = parent.realpath.to_s
+
+    unless path.to_s[0...parent.length] == parent
+      raise "Illegal path: #{path.to_s}, #{parent}"
+    end
+    
+    return path
+  end
+
 end
-
-
-  
